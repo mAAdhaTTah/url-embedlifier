9724b5177808cae0fcf1b3ab8893057bb9bace4d
mAAdhaTTah <jamesorodig@gmail.com>
2014-10-12 21:04:15 -0400

    Add version check

--- url-embedlifier.php
+++ url-embedlifier.php
@@ -30,6 +30,12 @@ if ( ! defined( 'WPINC' ) ) {
 	die;
 }
 
+// If we don't have the right PHP version, abort.
+if ( version_compare( PHP_VERSION, '5.3.0', '<' ) ) {
+	deactivate_plugins( basename( __FILE__ ) );
+	wp_die('<p><strong>SocialDen</strong> requires PHP  version 5.3 or greater.</p>', 'Plugin Activation Error',  array( 'response'=>200 ) );
+}
+
 /**
  * The code that runs during plugin activation.
  */
