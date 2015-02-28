578be9709088038a5cabbf2e13e40dc901cc5f5e
mAAdhaTTah <jamesorodig@gmail.com>
2015-02-28 18:18:01 +0100

    Fix for SSL

--- public/partials/video-display.php
+++ public/partials/video-display.php
@@ -42,7 +42,7 @@ if ( has_post_thumbnail() ) {
 
 	<div class="url-embedlified-footer">
 		<div class="embedly-powered">
-			<a target="_blank" href="http://embed.ly?src=anywhere" title="Powered by Embedly"><img src="http://static.embed.ly/images/logos/embedly-powered-small-light.png" alt="Embedly Powered" /></a>
+			<a target="_blank" href="//embed.ly?src=anywhere" title="Powered by Embedly"><img src="//static.embed.ly/images/logos/embedly-powered-small-light.png" alt="Embedly Powered" /></a>
 		</div>
 		<div class="url-embedlified-media-attribution">
 			<span>via </span><a href="<?php echo get_post_meta( get_the_ID(), 'embedlified_provider_url', true ); ?>" class="media-attribution-link" target="_blank"><?php echo get_post_meta( get_the_ID(), 'embedlified_provider_name', true ); ?></a>
